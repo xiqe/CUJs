8f15ee65858891c483cf7eba0dcbb20a64a5a6d8
Jerry <liangwq@glodon.com>
2018-05-23 19:36:08 +0800

    update

--- src/Func/Array.js
+++ src/Func/Array.js
@@ -1,4 +1,11 @@
 const _array = {
+    unique: (arr)=>{
+        let res = [];
+        res = arr.filter((item)=>{
+            return res.includes(item)?'':res.push(item);
+        })
+        return res;
+    }
 }
 
 export default _array;
